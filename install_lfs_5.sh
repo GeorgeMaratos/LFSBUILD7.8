f9ca15d4bc3c022ed9be46747b64fb043b6545d8
George <ffmpeg12341@gmail.com>
2015-10-14 12:40:35 -0500

    Added a few more steps, still in the main compilation

---
+++ install_lfs_5.sh
@@ -0,0 +1,297 @@
+
+#! /bin/bash
+set -e
+
+#binutils continued
+mkdir -v ../binutils-build
+cd ../binutils-build
+
+../binutils-2.25.1/configure --prefix=/usr   \
+                           --enable-shared \
+                           --disable-werror
+
+make tooldir=/usr
+
+make tooldir=/usr install
+
+
+#gmp-6.0.0a.tar.xz
+
+tar xf gmp-6.0.0a.tar.xz
+cd gmp-6.0.0a
+
+#mpfr-3.1.3.tar.xz
+
+tar xf mpfr-3.1.3.tar.xz
+cd mpfr-3.1.3
+
+#mpc-1.0.3.tar.gz
+
+tar xf mpc-1.0.3.tar.gz
+cd mpc-1.0.3
+
+#gcc-5.2.0.tar.bz2
+
+tar xf gcc-5.2.0.tar.bz2
+cd gcc-5.2.0
+
+#bzip2-1.0.6.tar.gz
+
+tar xf bzip2-1.0.6.tar.gz
+cd bzip2-1.0.6
+
+#pkg-config-0.28.tar.gz
+
+tar xf pkg-config-0.28.tar.gz
+cd pkg-config-0.28
+
+#ncurses-6.0.tar.gz
+
+tar xf ncurses-6.0.tar.gz
+cd ncurses-6.0
+
+#attr-2.4.47.src.tar.gz
+
+tar xf attr-2.4.47.src.tar.gz
+cd attr-2.4.47.src
+
+#acl-2.2.52.src.tar.gz
+
+tar xf acl-2.2.52.src.tar.gz
+cd acl-2.2.52.src
+
+#libcap-2.24.tar.xz
+
+tar xf libcap-2.24.tar.xz
+cd libcap-2.24
+
+#sed-4.2.2.tar.bz2
+
+tar xf sed-4.2.2.tar.bz2
+cd sed-4.2.2
+
+#shadow-4.2.1.tar.xz
+
+tar xf shadow-4.2.1.tar.xz
+cd shadow-4.2.1
+
+#psmisc-22.21.tar.gz
+
+tar xf psmisc-22.21.tar.gz
+cd psmisc-22.21
+
+#procps-ng-3.3.11.tar.xz
+
+tar xf procps-ng-3.3.11.tar.xz
+cd procps-ng-3.3.11
+
+#e2fsprogs-1.42.13.tar.gz
+
+tar xf e2fsprogs-1.42.13.tar.gz
+cd e2fsprogs-1.42.13
+
+#coreutils-8.24.tar.xz
+
+tar xf coreutils-8.24.tar.xz
+cd coreutils-8.24
+
+#iana-etc-2.30.tar.bz2
+
+tar xf iana-etc-2.30.tar.bz2
+cd iana-etc-2.30
+
+#m4-1.4.17.tar.xz
+
+tar xf m4-1.4.17.tar.xz
+cd m4-1.4.17
+
+#flex-2.5.39.tar.xz
+
+tar xf flex-2.5.39.tar.xz
+cd flex-2.5.39
+
+#bison-3.0.4.tar.xz
+
+tar xf bison-3.0.4.tar.xz
+cd bison-3.0.4
+
+#grep-2.21.tar.xz
+
+tar xf grep-2.21.tar.xz
+cd grep-2.21
+
+#readline-6.3.tar.gz
+
+tar xf readline-6.3.tar.gz
+cd readline-6.3
+
+#bash-4.3.30.tar.gz
+
+tar xf bash-4.3.30.tar.gz
+cd bash-4.3.30
+
+#bc-1.06.95.tar.bz2
+
+tar xf bc-1.06.95.tar.bz2
+cd bc-1.06.95
+
+#libtool-2.4.6.tar.xz
+
+tar xf libtool-2.4.6.tar.xz
+cd libtool-2.4.6
+
+#gdbm-1.11.tar.gz
+
+tar xf gdbm-1.11.tar.gz
+cd gdbm-1.11
+
+#expat-2.1.0.tar.gz
+
+tar xf expat-2.1.0.tar.gz
+cd expat-2.1.0
+
+#inetutils-1.9.4.tar.xz
+
+tar xf inetutils-1.9.4.tar.xz
+cd inetutils-1.9.4
+
+#perl-5.22.0.tar.bz2
+
+tar xf perl-5.22.0.tar.bz2
+cd perl-5.22.0
+
+#XML-Parser-2.44.tar.gz
+
+tar xf XML-Parser-2.44.tar.gz
+cd XML-Parser-2.44
+
+#autoconf-2.69.tar.xz
+
+tar xf autoconf-2.69.tar.xz
+cd autoconf-2.69
+
+#diffutils-3.3.tar.xz
+
+tar xf diffutils-3.3.tar.xz
+cd diffutils-3.3
+
+#gawk-4.1.3.tar.xz
+
+tar xf gawk-4.1.3.tar.xz
+cd gawk-4.1.3
+
+#findutils-4.4.2.tar.gz
+
+tar xf findutils-4.4.2.tar.gz
+cd findutils-4.4.2
+
+#gettext-0.19.5.1.tar.xz
+
+tar xf gettext-0.19.5.1.tar.xz
+cd gettext-0.19.5.1
+
+#intltool-0.51.0.tar.gz
+
+tar xf intltool-0.51.0.tar.gz
+cd intltool-0.51.0
+
+#gperf-3.0.4.tar.gz
+
+tar xf gperf-3.0.4.tar.gz
+cd gperf-3.0.4
+
+#groff-1.22.3.tar.gz
+
+tar xf groff-1.22.3.tar.gz
+cd groff-1.22.3
+
+#xz-5.2.1.tar.xz
+
+tar xf xz-5.2.1.tar.xz
+cd xz-5.2.1
+
+#grub-2.02~beta2.tar.xz
+
+tar xf grub-2.02~beta2.tar.xz
+cd grub-2.02~beta2
+
+#less-458.tar.gz
+
+tar xf less-458.tar.gz
+cd less-458
+
+#gzip-1.6.tar.xz
+
+tar xf gzip-1.6.tar.xz
+cd gzip-1.6
+
+#iproute2-4.2.0.tar.xz
+
+tar xf iproute2-4.2.0.tar.xz
+cd iproute2-4.2.0
+
+#kbd-2.0.3.tar.xz
+
+tar xf kbd-2.0.3.tar.xz
+cd kbd-2.0.3
+
+#kmod-21.tar.xz
+
+tar xf kmod-21.tar.xz
+cd kmod-21
+
+#libpipeline-1.4.1.tar.gz
+
+tar xf libpipeline-1.4.1.tar.gz
+cd libpipeline-1.4.1
+
+#make-4.1.tar.bz2
+
+tar xf make-4.1.tar.bz2
+cd make-4.1
+
+#patch-2.7.5.tar.xz
+
+tar xf patch-2.7.5.tar.xz
+cd patch-2.7.5
+
+#sysklogd-1.5.1.tar.gz
+
+tar xf sysklogd-1.5.1.tar.gz
+cd sysklogd-1.5.1
+
+#sysvinit-2.88dsf.tar.bz2
+
+tar xf sysvinit-2.88dsf.tar.bz2
+cd sysvinit-2.88dsf
+
+#tar-1.28.tar.xz
+
+tar xf tar-1.28.tar.xz
+cd tar-1.28
+
+#texinfo-6.0.tar.xz
+
+tar xf texinfo-6.0.tar.xz
+cd texinfo-6.0
+
+#eudev-3.1.2.tar.gz
+
+tar xf eudev-3.1.2.tar.gz
+cd eudev-3.1.2
+
+#util-linux-2.27.tar.xz
+
+tar xf util-linux-2.27.tar.xz
+cd util-linux-2.27
+
+#man-db-2.7.2.tar.xz
+
+tar xf man-db-2.7.2.tar.xz
+cd man-db-2.7.2
+
+#vim-7.4.tar.bz2
+
+tar xf vim-7.4.tar.bz2
+cd vim-7.4
+
